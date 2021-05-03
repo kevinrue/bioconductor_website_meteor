d52977a12f25266bae66de5e280b06fb22ecf505
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-05-03 15:29:32 +0100

    import bootstrap stylesheet

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BodyHeader } from './BodyHeader.jsx';
 import { BodyFooter } from './BodyFooter.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App = () => (
   <div>
