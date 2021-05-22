b88569f95ae6434fe4e16569bdd5b124f959ff8e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-05-22 19:07:35 +0100

    move form to navbar; login is not needed to see the page

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,32 +1,12 @@
-import React, { Fragment } from 'react';
-import { useTracker } from 'meteor/react-meteor-data';
-import { LoginForm } from './LoginForm';
+import React from 'react';
 import { BodyHeader } from './BodyHeader.jsx';
 import { BodyFooter } from './BodyFooter.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const App = () => {
-	const user = useTracker(() => Meteor.user());
-	const logout = () => Meteor.logout();
-
-	console.log(user)
-
-	return (
-
-      <div className="main">
-        {user ? (
-          <Fragment>
-          	<div className="user" onClick={logout}>
-              {user.username} 🚪
-            </div>
-
-            <BodyHeader />
-		    <h1>Welcome to Bioconductor!</h1>
-		    <BodyFooter />
-          </Fragment>
-        ) : (
-          <LoginForm />
-        )}
-      </div>
-	)
-}
+export const App = () => (
+  <div>
+    <BodyHeader />
+    <h1>Welcome to Bioconductor!</h1>
+    <BodyFooter />
+  </div>
+);
--- imports/ui/BodyHeader.jsx
+++ imports/ui/BodyHeader.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import { useTracker } from 'meteor/react-meteor-data';
+import { LoginForm } from './LoginForm';
 
 export const BodyHeader = () => {
+    const user = useTracker(() => Meteor.user());
+    const logout = () => Meteor.logout();
+
+    console.log(user);
 
     return (
         <Navbar className="bioconductor-nav">
             <Navbar.Brand href="#home"><img src="logo/bioconductor.gif" height="100%" /></Navbar.Brand>
+            
             <Nav className="mr-auto">
                 <Nav.Link href="#news">Latest News</Nav.Link>
                 <Nav.Link href="#packages">Packages</Nav.Link>
                 <Nav.Link href="#support">Support</Nav.Link>
             </Nav>
+            
             <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                 <Button variant="outline-info">Search</Button>
             </Form>
+
+            {user ? (
+              <Fragment>
+                {user.username} 
+                <div className="user" onClick={logout}>
+                  🚪
+                </div>
+              </Fragment>
+            ) : (
+              <LoginForm />
+            )}
+
         </Navbar>
     );
 };
