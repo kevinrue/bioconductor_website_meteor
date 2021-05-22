9366759288e9fb6de466bcc6daad79f938c2e140
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-05-22 13:24:52 +0100

    Add basic account management

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { LoginForm } from './LoginForm';
 import { BodyHeader } from './BodyHeader.jsx';
 import { BodyFooter } from './BodyFooter.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const App = () => (
-  <div>
-    <BodyHeader />
-    <h1>Welcome to Bioconductor!</h1>
-    <BodyFooter />
-  </div>
-);
+export const App = () => {
+	const user = useTracker(() => Meteor.user());
+	const logout = () => Meteor.logout();
+
+	console.log(user)
+
+	return (
+
+      <div className="main">
+        {user ? (
+          <Fragment>
+          	<div className="user" onClick={logout}>
+              {user.username} 🚪
+            </div>
+
+            <BodyHeader />
+		    <h1>Welcome to Bioconductor!</h1>
+		    <BodyFooter />
+          </Fragment>
+        ) : (
+          <LoginForm />
+        )}
+      </div>
+	)
+}
