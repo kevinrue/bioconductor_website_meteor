36e6ff2629eaa0d1a96a9b892a728c51e7fedc54
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-05-25 09:27:55 +0100

    move navbar and footer to components

---
+++ imports/ui/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => {
+
+    return (
+        <div>
+            This will be the footer.
+        </div>
+    );
+};
---
+++ imports/ui/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import { useTracker } from 'meteor/react-meteor-data';
+
+export const NavBar = () => {
+    const user = useTracker(() => Meteor.user());
+    const logout = () => Meteor.logout();
+
+    console.log(user);
+
+    return (
+        <Navbar className="bioconductor-nav">
+            <Navbar.Brand href="/"><img src="logo/bioconductor.gif" height="100%" /></Navbar.Brand>
+            
+            <Nav className="mr-auto">
+                <Nav.Link href="/news">Latest News</Nav.Link>
+                <Nav.Link href="/packages">Packages</Nav.Link>
+                <Nav.Link href="/support">Support</Nav.Link>
+            </Nav>
+            
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-info">Search</Button>
+            </Form>
+
+            {user ? (
+              <Fragment>
+                <NavDropdown alignRight title="Profile" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="/profile">Signed in as <strong>{user.username}</strong></NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/logout">
+                        <div className="user" onClick={logout}>
+                            Sign out
+                        </div></NavDropdown.Item>
+                </NavDropdown>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <Nav className="justify-content-end">
+                    <Nav.Link href="/signin">Sign in</Nav.Link>
+                </Nav>
+              </Fragment>
+            )}
+
+        </Navbar>
+    );
+};
