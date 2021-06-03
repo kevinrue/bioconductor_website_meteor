bb45b24f988c25e968b2c500aefd2deaf8d434ae
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-06-03 16:47:26 +0100

    restrict information about users published by server

--- imports/api/usersPublications.jsx
+++ imports/api/usersPublications.jsx
@@ -2,5 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { UsersCollection } from '/imports/db/UsersCollection';
 
 Meteor.publish('users', function publishUsers() {
-  return UsersCollection.find({});
+  return UsersCollection.find({}, {
+    fields: { 'profile.name': 1, username: 1 }
+  });
 });
